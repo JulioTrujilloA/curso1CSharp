46d6b5f7cefc7c8ef2560b3b2c9a84126d7f1406
Julio Trujillo <julio.trujillo.alvarado@outlook.com>
2015-05-07 15:25:39 -0500

    primer programa. Video2

---
+++ Programa1/Programa1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Primer programa en Do Sostenido. Comentario de línea.
+            /*
+             * Nombre: Julio Trujillo
+             * Fecha: 07/05/2015
+             * Hora: 14:45
+             */
+
+            // Línea que muestra un mensaje en la consola
+            Console.WriteLine("El clásico inicio de programador primerizo...");
+            
+            // Línea que detiene el programa para que reciba un teclazo
+            Console.ReadKey();
+        }
+    }
+}
